b359ff7c152167c2298ae1634fe47b4c260d09aa
shenfive@gmail.com <Danny Shen>
2022-10-29 16:54:41 +0800

    LoginIn

--- A209FBLoing/AppDelegate.swift
+++ A209FBLoing/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import FacebookLogin
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,9 +15,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         // Override point for customization after application launch.
+        
+        ApplicationDelegate.shared.application(
+            application,
+            didFinishLaunchingWithOptions: launchOptions
+        )
+        
+        
         return true
     }
+    
+    
+    func application(
+        _ app: UIApplication,
+        open url: URL,
+        options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+    ) -> Bool {
+        ApplicationDelegate.shared.application(
+            app,
+            open: url,
+            sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+            annotation: options[UIApplication.OpenURLOptionsKey.annotation]
+        )
+    }
+    
+    
+    
 
     // MARK: UISceneSession Lifecycle
 
@@ -34,3 +61,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+
+//
+//
+//// AppDelegate.swift
+//import UIKit
+//import FacebookCore
+//
+//@UIApplicationMain
+//class AppDelegate: UIResponder, UIApplicationDelegate {
+//func application(
+//    _ application: UIApplication,
+//    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+//) -> Bool {
+//    ApplicationDelegate.shared.application(
+//        application,
+//        didFinishLaunchingWithOptions: launchOptions
+//    )
+//
+//    return true
+//}
+//
+
+//}
--- A209FBLoing/ViewController.swift
+++ A209FBLoing/ViewController.swift
@@ -6,14 +6,53 @@
 //
 
 import UIKit
+import FacebookLogin
+import Firebase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, LoginButtonDelegate {
 
+    
+
+    
+    let facebookLoginBtn = FBLoginButton()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        facebookLoginBtn.delegate = self
+       
+        facebookLoginBtn.center = self.view.center
+        facebookLoginBtn.permissions = ["public_profile", "email"]
+        self.view.addSubview(facebookLoginBtn)
     }
 
+    //MARK:FBLogin Delegate
+    func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?) {
+        if let error = error{
+            print(error.localizedDescription)
+            return
+        }
+        if let token = AccessToken.current{
+            let credential = FacebookAuthProvider
+                .credential(withAccessToken: token.tokenString)
+            
+            Auth.auth().signIn(with: credential) { authResult, error in
+                if let error = error {
+                    print(error.localizedDescription)
+                }
+            }
+        }else{
+            print("登入失敗")
+        }
+        
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
+        logout()
+    }
+    
+    func logout(){
+        
+    }
 
 }
 
